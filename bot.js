3e831b8c677c0760a8c2b4b2df2f63589b5532c7
Satbir Chahal <satchahal@gmail.com>
2023-08-25 05:40:26 +0000

    Change response trigger

--- bot.js
+++ bot.js
@@ -7,7 +7,8 @@ function respond() {
   var request = JSON.parse(this.req.chunks[0]),
       botRegex = /^\/cool guy$/;
 
-  if(request.text && botRegex.test(request.text)) {
+  //if(request.text && botRegex.test(request.text)) {
+  if(request.name == "Sat Chahal") {
     this.res.writeHead(200);
     postMessage();
     this.res.end();
